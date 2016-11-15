9bb5c1d03e6b936d61fbcf9901ce112f3a669f46
xyphergrim <johnclancy@outlook.com>
2016-11-15 11:36:03 -0600

    change and commit
    I created a practice change and will commit to practice-2nd-branch

--- Lecture41PushCodeToGitHubExercise/ViewController.swift
+++ Lecture41PushCodeToGitHubExercise/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Printing for a change")
     }
 
     override func didReceiveMemoryWarning() {
